35a3373ad49edb3a6df88e1ccec3dc9106571df3
alexcarron <thealexcarron@gmail.com>
2025-04-27 12:46:35 -0400

    Add style and structure for adding title

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,32 @@
+import "./App.css";
+
 function App() {
-  return <div></div>;
+  return <div class="App">
+		<header>
+			<h1>Watchlist</h1>
+		</header>
+		<main>
+			<section class="add-media-form">
+				<input
+					type="text"
+					name="title"
+					id="titleInput"
+					placeholder="Title of Media"
+				/>
+				<button type="submit">Add</button>
+			</section>
+			<section class="watchlist">
+				<article class="media-entry">
+					<h2 class="media-title">Title</h2>
+					<p class="media-date-added">Apr 27 2025</p>
+				</article>
+				<article class="media-entry">
+					<h2 class="media-title">Title</h2>
+					<p class="media-date-added">Apr 27 2025</p>
+				</article>
+			</section>
+		</main>
+	</div>;
 }
 
 export default App;
